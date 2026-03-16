a365c4c75c380dc24e6a13ea90d649d8bface189
Rajeev <rajbudgule143@gmail.com>
2026-03-16 11:23:13 +0530

    add commend line function

--- bankapp/views.py
+++ bankapp/views.py
@@ -1,20 +1,19 @@
-from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render,redirect
 from .models import Account
 from .forms import AccountForm
 
-
+# this is the home function 
 def home(request):
     return render(request,'bankapp/home.html')
 
-
+# this is the account list function
 def account_list(request):
     accounts = Account.objects.all()
     return render(request,'bankapp/account_list.html',{'accounts':accounts})
 
-
+# this is the add account detail
 def add_account(request):
     form = AccountForm(request.POST or None)
 
@@ -24,7 +23,7 @@ def add_account(request):
 
     return render(request,'bankapp/add_account.html',{'form':form})
 
-
+# this is the update account 
 def update_account(request,id):
     account = Account.objects.get(id=id)
     form = AccountForm(request.POST or None,instance=account)
@@ -35,7 +34,7 @@ def update_account(request,id):
 
     return render(request,'bankapp/update_account.html',{'form':form})
 
-
+# this is the delete account 
 def delete_account(request,id):
     account = Account.objects.get(id=id)
     account.delete()
